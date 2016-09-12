7d78d0b6460e658ef485126bc9a0b04b5439c62d
kiritAyya <kirit.ayya@gmail.com>
2016-09-12 13:40:56 +0530

    user info

--- app/home.component.ts
+++ app/home.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'home',
-  template: `
-    <h1>Welcome to Auth0 - Angular 2 integration app</h1>
-  `
+  templateUrl: './home.template.html'
 })
 
 export class HomeComponent {}
